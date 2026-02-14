5e17a08899defc1e710b41351b2d3607b0ecf38e
Wogari-GH6877 <wogariararsa0968@gmail.com>
2026-02-13 23:27:36 -0800

    useContext problem is fixed

--- frontend1/src/components/AiAssistant.jsx
+++ frontend1/src/components/AiAssistant.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef ,useContext} from 'react';
 import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
 import { AppContext } from '../context/AppContext.jsx';
